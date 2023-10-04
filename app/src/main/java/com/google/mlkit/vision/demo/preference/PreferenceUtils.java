9606ba107ceaef8d37041af67aa1b8058b6c31aa
AriSepp <118427451+AriSepp@users.noreply.github.com>
2023-10-04 11:13:22 +0300

    preferecet siivottu

--- app/src/main/java/com/google/mlkit/vision/demo/preference/PreferenceUtils.java
+++ app/src/main/java/com/google/mlkit/vision/demo/preference/PreferenceUtils.java
@@ -34,7 +34,6 @@ import com.google.mlkit.vision.demo.R;
 /** Utility class to retrieve shared preferences. */
 public class PreferenceUtils {
 
-  private static final int POSE_DETECTOR_PERFORMANCE_MODE_FAST = 1;
 
   static void saveString(Context context, @StringRes int prefKeyId, @Nullable String value) {
     PreferenceManager.getDefaultSharedPreferences(context)
@@ -129,61 +128,15 @@ public class PreferenceUtils {
     return sharedPreferences.getBoolean(prefKey, false);
   }
 
-  public static boolean preferGPUForPoseDetection(Context context) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey = context.getString(R.string.pref_key_pose_detector_prefer_gpu);
-    return sharedPreferences.getBoolean(prefKey, true);
-  }
 
-  public static boolean shouldShowPoseDetectionInFrameLikelihoodLivePreview(Context context) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey =
-        context.getString(R.string.pref_key_live_preview_pose_detector_show_in_frame_likelihood);
-    return sharedPreferences.getBoolean(prefKey, true);
-  }
 
-  public static boolean shouldShowPoseDetectionInFrameLikelihoodStillImage(Context context) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey =
-        context.getString(R.string.pref_key_still_image_pose_detector_show_in_frame_likelihood);
-    return sharedPreferences.getBoolean(prefKey, true);
-  }
-
-  public static boolean shouldPoseDetectionVisualizeZ(Context context) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey = context.getString(R.string.pref_key_pose_detector_visualize_z);
-    return sharedPreferences.getBoolean(prefKey, true);
-  }
-
-  public static boolean shouldPoseDetectionRescaleZForVisualization(Context context) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey = context.getString(R.string.pref_key_pose_detector_rescale_z);
-    return sharedPreferences.getBoolean(prefKey, true);
-  }
-
-  public static boolean shouldPoseDetectionRunClassification(Context context) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey = context.getString(R.string.pref_key_pose_detector_run_classification);
-    return sharedPreferences.getBoolean(prefKey, false);
-  }
-
-  public static boolean shouldSegmentationEnableRawSizeMask(Context context) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey = context.getString(R.string.pref_key_segmentation_raw_size_mask);
-    return sharedPreferences.getBoolean(prefKey, false);
-  }
 
   /**
    * Mode type preference is backed by {@link android.preference.ListPreference} which only support
    * storing its entry value as string type, so we need to retrieve as string and then convert to
    * integer.
    */
-  private static int getModeTypePreferenceValue(
-      Context context, @StringRes int prefKeyResId, int defaultValue) {
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-    String prefKey = context.getString(prefKeyResId);
-    return Integer.parseInt(sharedPreferences.getString(prefKey, String.valueOf(defaultValue)));
-  }
+
 
   public static boolean isCameraLiveViewportEnabled(Context context) {
     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
