353184a899fea0174569222616be2b57f8723e75
AriSepp <118427451+AriSepp@users.noreply.github.com>
2023-11-01 14:32:52 +0200

    main = simple login
    Login button -> ChooserActivity

--- app/src/main/java/com/google/mlkit/vision/demo/SimpleLoginActivity.java
+++ app/src/main/java/com/google/mlkit/vision/demo/SimpleLoginActivity.java
@@ -1,7 +1,12 @@
+package com.google.mlkit.vision.demo;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.mlkit.vision.demo.EntryChoiceActivity;
 import com.google.mlkit.vision.demo.R;
 
 public class SimpleLoginActivity extends AppCompatActivity {
@@ -12,17 +17,20 @@ public class SimpleLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.valid_ui);
 
-        buttonLogin = buttonLogin.findViewById();
+        buttonLogin = findViewById(R.id.buttonLogin); // Find your button by its ID
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Handle the button click event here
                 // You can navigate to another activity or perform any other action.
+                navigateToChooserActivity(); // Call the method to navigate to ChooserActivity
             }
         });
     }
 
-    private void setContentView(int validUi) {
+    private void navigateToChooserActivity() {
+        Intent intent = new Intent(this, EntryChoiceActivity.class);
+        startActivity(intent);
     }
 }
