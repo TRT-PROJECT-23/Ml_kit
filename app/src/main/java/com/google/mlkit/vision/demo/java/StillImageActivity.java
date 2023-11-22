2fced29505bc4878505f075926de1ae167ef1bf0
AriSepp <118427451+AriSepp@users.noreply.github.com>
2023-11-22 18:40:27 +0200

    nada

--- app/src/main/java/com/google/mlkit/vision/demo/java/StillImageActivity.java
+++ app/src/main/java/com/google/mlkit/vision/demo/java/StillImageActivity.java
@@ -322,7 +322,7 @@ public final class StillImageActivity extends AppCompatActivity {
       Bitmap binaryBitmap = convertToBinary(processedBitmap, 128);
 
       // Apply blur effect to the processed image
-      Bitmap blurredBitmap = applyBlur(getApplicationContext(),processedBitmap, 15.0f); // Adjust blur radius
+      Bitmap blurredBitmap = applyBlur(getApplicationContext(),processedBitmap, 18.0f); // Adjust blur radius
 
 
       // Clear the overlay first
