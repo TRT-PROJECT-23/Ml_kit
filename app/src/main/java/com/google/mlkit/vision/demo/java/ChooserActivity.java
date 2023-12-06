cbfd01d754b5e474c321d3859ff058ad2af5581f
AriSepp <118427451+AriSepp@users.noreply.github.com>
2023-12-06 21:20:16 +0200

    hienosäätö UI
    chooser bg + nimet
    still painike keskelle

--- app/src/main/java/com/google/mlkit/vision/demo/java/ChooserActivity.java
+++ app/src/main/java/com/google/mlkit/vision/demo/java/ChooserActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2020 Google LLC. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.mlkit.vision.demo.java;
 
 import android.content.Context;
@@ -35,8 +19,7 @@ import com.google.mlkit.vision.demo.BuildConfig;
 import com.google.mlkit.vision.demo.R;
 
 /** Demo app chooser which allows you pick from all available testing Activities. */
-public final class ChooserActivity extends AppCompatActivity
-    implements AdapterView.OnItemClickListener {
+public final class ChooserActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
   private static final String TAG = "ChooserActivity";
 
   @SuppressWarnings("NewApi") // CameraX is only available on API 21+
@@ -46,28 +29,35 @@ public final class ChooserActivity extends AppCompatActivity
           };
 
   private static final int[] DESCRIPTION_IDS =
-      VERSION.SDK_INT < VERSION_CODES.LOLLIPOP
-          ? new int[] {
-            R.string.desc_camera_source_activity, R.string.desc_still_image_activity,
+          VERSION.SDK_INT < VERSION_CODES.LOLLIPOP
+                  ? new int[]{
+                  R.string.desc_camera_source_activity, R.string.desc_still_image_activity,
           }
-          : new int[] {
-            R.string.desc_camera_source_activity,
-            R.string.desc_still_image_activity,
-            R.string.desc_camerax_live_preview_activity,
-            R.string.desc_cameraxsource_demo_activity,
+                  : new int[]{
+                  R.string.desc_camera_source_activity,
+                  R.string.desc_still_image_activity,
+                  R.string.desc_camerax_live_preview_activity,
+                  R.string.desc_cameraxsource_demo_activity,
+          };
+
+  // Add custom names array to hold the names to be displayed in the list
+  private static final String[] CUSTOM_NAMES =
+          new String[]{
+                  "Blood Glucose",
+                  // Add custom names for additional activities if needed
           };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     if (BuildConfig.DEBUG) {
       StrictMode.setThreadPolicy(
-          new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+              new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
       StrictMode.setVmPolicy(
-          new StrictMode.VmPolicy.Builder()
-              .detectLeakedSqlLiteObjects()
-              .detectLeakedClosableObjects()
-              .penaltyLog()
-              .build());
+              new StrictMode.VmPolicy.Builder()
+                      .detectLeakedSqlLiteObjects()
+                      .detectLeakedClosableObjects()
+                      .penaltyLog()
+                      .build());
     }
     super.onCreate(savedInstanceState);
     Log.d(TAG, "onCreate");
@@ -80,6 +70,9 @@ public final class ChooserActivity extends AppCompatActivity
     MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES);
     adapter.setDescriptionIds(DESCRIPTION_IDS);
 
+    // Set custom names in the adapter
+    adapter.setCustomNames(CUSTOM_NAMES);
+
     listView.setAdapter(adapter);
     listView.setOnItemClickListener(this);
   }
@@ -91,10 +84,10 @@ public final class ChooserActivity extends AppCompatActivity
   }
 
   private static class MyArrayAdapter extends ArrayAdapter<Class<?>> {
-
     private final Context context;
     private final Class<?>[] classes;
     private int[] descriptionIds;
+    private String[] customNames;
 
     MyArrayAdapter(Context context, int resource, Class<?>[] objects) {
       super(context, resource, objects);
@@ -109,11 +102,16 @@ public final class ChooserActivity extends AppCompatActivity
 
       if (convertView == null) {
         LayoutInflater inflater =
-            (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+                (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
         view = inflater.inflate(android.R.layout.simple_list_item_2, null);
       }
 
-      ((TextView) view.findViewById(android.R.id.text1)).setText(classes[position].getSimpleName());
+      // Get the display name: custom name or default class name
+      String displayName = (customNames != null && customNames.length > position)
+              ? customNames[position]
+              : classes[position].getSimpleName();
+
+      ((TextView) view.findViewById(android.R.id.text1)).setText(displayName);
       ((TextView) view.findViewById(android.R.id.text2)).setText(descriptionIds[position]);
 
       return view;
@@ -121,6 +119,11 @@ public final class ChooserActivity extends AppCompatActivity
 
     void setDescriptionIds(int[] descriptionIds) {
       this.descriptionIds = descriptionIds;
+    }
+
+    // Method to set custom names
+    void setCustomNames(String[] customNames) {
+      this.customNames = customNames;
     }
   }
 }
